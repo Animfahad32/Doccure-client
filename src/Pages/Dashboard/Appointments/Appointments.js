af869c325d06d6a8eb598b57ac19d7e164d78ba1
animfahad32 <animfahad32@gmail.com>
2021-11-09 21:18:53 +0600

    date issue fixed

--- src/Pages/Dashboard/Appointments/Appointments.js
+++ src/Pages/Dashboard/Appointments/Appointments.js
@@ -14,7 +14,7 @@ const Appointments = ({date}) => {
     const [appointments, setAppointments] =useState([])
 
     useEffect(()=>{
-        const url= `https://sheltered-dusk-10770.herokuapp.com/appointments?email=${user.email}&date=${date}`
+        const url= `https://sheltered-dusk-10770.herokuapp.com/appointments?email=${user.email}&date=${date.toLocaleDateString()}`
       fetch(url, {
         header: {
           'authorization': `Bearer ${token}`
